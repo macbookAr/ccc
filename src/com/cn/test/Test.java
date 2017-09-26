003ac5128cc68ee00bf161b47ede4ad8e907ba59
macbookAr <zmm_job_sz@163.com>
2017-09-26 20:30:22 +0800

    222

--- src/com/cn/test/Test.java
+++ src/com/cn/test/Test.java
@@ -2,7 +2,8 @@ package com.cn.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("哈哈哈哈，成功啦！");
+		System.out.println("哈哈哈哈哈哈或或或或或");
+		System.out.println("黑呵呵");
 	}
 
 }
